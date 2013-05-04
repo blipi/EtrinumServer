0ee3c05bc3a6a8725dacc304678b4b54539d6017
Blipi <gpascualg93@gmail.com>
2013-05-04 03:56:02 +0200

    CMake, Core: Code portability for UNIX, Step III

--- src/Common/debugging.h
+++ src/Common/debugging.h
@@ -7,8 +7,4 @@
 
 #define ASSERT(assertion) { if (!(assertion)) { Poco::NDC::current().dump(std::cerr); *((volatile int*)NULL) = 0; } }
 
-// Memory Leaks
-#include <stdlib.h>
-#include <crtdbg.h>
-
 #endif
--- src/Database/Database.h
+++ src/Database/Database.h
@@ -53,7 +53,7 @@ public:
 };
 
 template <>
-class TypeHandler<class PreparedStatementData>
+class TypeHandler<PreparedStatementData>
 {
 public:
 
