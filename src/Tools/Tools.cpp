242901640c2d1ca5032c8e085f850e2548348326
Blipi <gpascualg93@gmail.com>
2013-05-04 04:08:00 +0200

    Core/Tools: Correctly use headers, UNIX portability, Step VI

--- src/Tools/Tools.cpp
+++ src/Tools/Tools.cpp
@@ -1,6 +1,8 @@
 #include "Tools.h"
-
 #include "defines.h"
+
+#include <string.h>
+#include <cmath>
 
 namespace Tools
 {
@@ -20,17 +22,17 @@ namespace Tools
     float u32tof(Poco::UInt32 value)
 	{
 		float f;
-		memcpy( &f, &value, sizeof(Poco::UInt32));
+		memcpy(&f, &value, sizeof(Poco::UInt32));
 		return f;
 	}
 
     Poco::UInt16 GetCellFromPos(double p)
     {
-        return (int)(p/UNITS_PER_CELL) + 1;
+        return (int)(p / UNITS_PER_CELL) + 1;
     }
 
     Poco::UInt16 GetPositionInCell(Poco::UInt16 cell, float b)
     {
-        return Poco::UInt16(abs(((cell - 1) * UNITS_PER_CELL) - b));
+        return Poco::UInt16(std::abs(((cell - 1) * UNITS_PER_CELL) - b));
     }
 }
